73a795ff467730d85d8141f41b9575ef1e52e3e1
Luan Dantas <luandc29@gmail.com>
2022-05-03 15:14:12 -0300

    aperfeiçoamento

--- script.js
+++ script.js
@@ -1,122 +1,123 @@
-const transactionsUl = document.querySelector('#transactions')
-const incomeDisplay = document.querySelector('#money-plus')
-const expenseDisplay = document.querySelector('#money-minus')
-const balanceDisplay = document.querySelector('#balance')
-const form = document.querySelector('#form')
-const inputTransactionName = document.querySelector('#text')
-const inputTransactionAmount = document.querySelector('#amount')
 
-// comando de amrmazenamento das transações 
+const app = (function() {
+    const transactionsUl = document.querySelector('#transactions');
+    const incomeDisplay = document.querySelector('#money-plus');
+    const expenseDisplay = document.querySelector('#money-minus');
+    const balanceDisplay = document.querySelector('#balance');
+    const form = document.forms.transacao;
+    const { amount, text } = form;
 
-const localStorageTransactions = JSON.parse(localStorage
-    .getItem('transactions'))
-let transactions = localStorage
-.getItem('transactions') !== null ? localStorageTransactions : []
-
-// comando para remoção de transações
-
-const removeTransaction = ID => {
-    transactions = transactions.filter(transaction => 
-        transaction.id !== ID)
-    updateLocalStorage()
-    init()
-
+    const toCurrency = value => value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     
-}
+    // comando de amrmazenamento das transações 
 
-// declaração de classes html e css para utilizar no js para inserção de valores nos elementos HTML
+    const localStorageTransactions = JSON.parse(localStorage
+        .getItem('transactions'));
+    let transactions = localStorage
+    .getItem('transactions') !== null ? localStorageTransactions : [];
 
-const addTransactionIntoDOM = ({amount, name, id}) => {
-    const operator = amount <0 ? '-':'+'
-    const CSSClass = amount <0 ? 'minus':'plus'
-    const amountWithoutOperator = Math.abs(amount)
-    const li = document.createElement('li')
+    // comando para remoção de transações
 
-    li.classList.add(CSSClass)
-    li.innerHTML =`
-    ${name} 
-    <span>${operator}R$ ${amountWithoutOperator}</span>
-    <button class="delete-btn" onclick ="removeTransaction(${id})">x</button>`
-transactionsUl.append(li)
-}
-
-// atualização de valores relativos a entradas de dados
-const getTotal = transactionsAmounts => transactionsAmounts
-.reduce((acumulator, transaction) => acumulator + transaction, 0)
-.toFixed(2)
-
-const getIncome = transactionsAmounts => transactionsAmounts
-.filter(value => value > 0)
-.reduce((acumulator, value) => acumulator + value, 0)
-.toFixed(2)
-
-const getExpense = transactionsAmounts =>Math.abs(transactionsAmounts
-        .filter(value => value < 0)
-        .reduce((acumulator, value) => acumulator + value, 0))
-        .toFixed(2) 
-
-const updateBalanceValues = () => {
-    const transactionsAmounts = transactions.map(({amount}) => amount)
-    const total = getTotal(transactionsAmounts)
-    const income = getIncome(transactionsAmounts)
-    const expense = getExpense(transactionsAmounts)
-
-
-        incomeDisplay.textContent = `R$ ${income}`
-        balanceDisplay.textContent = `R$ ${total}`
-        expenseDisplay.textContent = `R$ ${expense}`
-}
-
-// apresentação da lista de itens adicionados na pagina
-
-const init = () => {   
-    transactionsUl.innerHTML = '' 
-    transactions.forEach(addTransactionIntoDOM)
-    updateBalanceValues()
-}
-
-init()
-
-const updateLocalStorage = () => {
-   localStorage.setItem('transactions', JSON.stringify(transactions))
-}
-
-const generateID = () => Math.round(Math.random() * 1000)
-
-const clearInputs = () => {
-    inputTransactionAmount.value = ''
-    inputTransactionName.value = '' 
-}
-
-const addToTransactionArray = (TransactionName, TransactionAmount) => {
-    transactions.push({ 
-        id: generateID(), 
-        name: TransactionName, 
-        amount: Number(TransactionAmount) 
-    })
-}
-
-const heandleFormSubmit = event => {
-    event.preventDefault()
-    
-    const TransactionName = inputTransactionName.value.trim()
-    const TransactionAmount = inputTransactionAmount.value.trim()
-    const isSomeInputEmpty = TransactionName === '' || TransactionAmount === ''
-
-    if(isSomeInputEmpty) {
-        alert('Por favor, preencha os campos em branco')
-        return
+    const removeTransaction = ID => {
+        transactions = transactions.filter(transaction => 
+            transaction.id !== ID);
+        updateLocalStorage();
+        init();
     }
 
-    addToTransactionArray(TransactionName, TransactionAmount)
+    // declaração de classes html e css para utilizar no js para inserção de valores nos elementos HTML
+
+    const addTransactionIntoDOM = ({amount, name, id}) => {
+        const operator = amount <0 ? '-':'+';
+        const CSSClass = amount <0 ? 'minus':'plus';
+        const amountWithoutOperator = Math.abs(amount);
+        const li = document.createElement('li');
+
+        li.classList.add(CSSClass);
+        li.innerHTML =`
+        ${name} 
+        <span>${operator}R$ ${amountWithoutOperator}</span>
+        <button class="delete-btn" onclick ="app.removeTransaction(${id})">x</button>`;
+        transactionsUl.append(li);
+    }
+
+    // atualização de valores relativos a entradas de dados
+    const getTotal = transactionsAmounts => transactionsAmounts
+        .reduce((acumulator, transaction) => acumulator + transaction, 0)
+        .toFixed(2);
+
+    const getIncome = transactionsAmounts => transactionsAmounts
+        .filter(value => value > 0)
+        .reduce((acumulator, value) => acumulator + value, 0)
+        .toFixed(2);
+
+    const getExpense = transactionsAmounts => toCurrency(
+        Math.abs(transactionsAmounts
+        .filter(value => value < 0)
+        .reduce((acumulator, value) => acumulator + value, 0))
+    );
+
+    const updateBalanceValues = () => {
+        const transactionsAmounts = transactions.map(({amount}) => amount);
+        const total = getTotal(transactionsAmounts);
+        const income = getIncome(transactionsAmounts);
+        const expense = getExpense(transactionsAmounts);
+
+        incomeDisplay.textContent = toCurrency(income);
+        balanceDisplay.textContent = toCurrency(total);
+        expenseDisplay.textContent = toCurrency(expense);
+    }
+
+    // apresentação da lista de itens adicionados na pagina
+
+    const init = () => {   
+        transactionsUl.innerHTML = '' ;
+        transactions.forEach(addTransactionIntoDOM);
+        updateBalanceValues();
+    }
+
     init()
-    updateLocalStorage()
-    clearInputs()
 
-    
-}
-form.addEventListener('submit', heandleFormSubmit)
+    const updateLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+    }
 
-    
-    
+    const generateID = () => Math.round(Math.random() * 1000);
 
+    const clearInputs = () => {
+        amount.value = '';
+        text.value = '';
+    }
+
+    const addToTransactionArray = (TransactionName, TransactionAmount) => {
+        transactions.push({ 
+            id: generateID(), 
+            name: TransactionName, 
+            amount: Number(TransactionAmount) 
+        });
+    }
+
+    const heandleFormSubmit = event => {
+        event.preventDefault()
+        
+        const TransactionName = text.value.trim();
+        const TransactionAmount = amount.value.trim();
+        const isSomeInputEmpty = TransactionName === '' || TransactionAmount === '';
+
+        if(isSomeInputEmpty) {
+            alert('Por favor, preencha os campos em branco');
+            return;
+        }
+
+        addToTransactionArray(TransactionName, TransactionAmount);
+        init();
+        updateLocalStorage();
+        clearInputs();
+    }
+    
+    form.addEventListener('submit', heandleFormSubmit);
+
+    return {
+        removeTransaction
+    };
+})();
